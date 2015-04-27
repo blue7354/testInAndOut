37c5fd621a4175c7b48cd4f941608aeefbdd88a3
blue7354 <xuan@xuandeMacBook-Pro.local>
2015-04-27 16:14:56 +0800

    V1.2

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,7 @@
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
-                var myQuery = new AV.Query(myData);
+                var myQuery = new AV.Query('myData');
                 myQuery.find({
                              success:function(results) {
                                  response.success(results);
